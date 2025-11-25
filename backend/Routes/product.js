54553281d78491bc6fd242c2cda677bd3aa091a5
Sushil Kumar <100270367+kumarsushilroy@users.noreply.github.com>
2025-11-25 21:40:32 +0530

    worked on file upload

--- backend/Routes/product.js
+++ backend/Routes/product.js
@@ -3,7 +3,7 @@ const express = require('express');
 const {createProduct,allProducts, singleProduct} = require('../Controllers/product');
 const {authorizeRoles, authenticatedUser} = require('../middleware.js');
 const router = express.Router();
-const upload = require('../Controllers/auth.js');
+const {upload} = require('../Controllers/auth.js');
 
 router.post('/create/product', upload.single('photo'), authenticatedUser, authorizeRoles('admin'), createProduct);
 router.get('/get/products', allProducts);
